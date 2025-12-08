f447c1f8b43aac0b25c127a3fc01590bf92f767c
phwnganh <phuonganh25102003@gmail.com>
2025-12-08 14:45:13 +0700

    feat: responsive key features section

--- ecommerce-website-demo/src/pages/HomePage/KeyFeatures.tsx
+++ ecommerce-website-demo/src/pages/HomePage/KeyFeatures.tsx
@@ -3,7 +3,7 @@ import CustomerServiceIcon from "../../assets/Icon-Customer service.svg";
 import SecureIcon from "../../assets/Icon-secure.svg";
 const KeyFeatures = () => {
   return (
-    <div className="max-w-[1170px] mx-auto flex justify-center gap-22 my-35 p-4 lg:p-0">
+    <div className="max-w-[1170px] mx-auto flex justify-center gap-12 md:gap-22 my-35 p-4 lg:p-0">
       <div className="flex flex-col gap-2 items-center">
         <div className="rounded-full bg-[#2F2E30] p-3 w-20 h-20">
           <div className="rounded-full bg-black p-2">
@@ -12,8 +12,8 @@ const KeyFeatures = () => {
             </div>
           </div>
         </div>
-        <div className="text-xl font-semibold mt-4">FREE AND FAST DELIVERY</div>
-        <div className="text-sm">Free delivery for all orders over $140</div>
+        <div className="text-base md:text-xl font-semibold mt-4 text-center lg:text-start">FREE AND FAST DELIVERY</div>
+        <div className="text-xs md:text-sm text-center lg:text-start">Free delivery for all orders over $140</div>
       </div>
 
       <div className="flex flex-col gap-2 items-center">
@@ -24,8 +24,8 @@ const KeyFeatures = () => {
             </div>
           </div>
         </div>
-        <div className="text-xl font-semibold mt-4">24/7 CUSTOMER SERVICE</div>
-        <div className="text-sm">Friendly 24/7 customer support</div>
+        <div className="text-base md:text-xl font-semibold mt-4 text-center lg:text-start">24/7 CUSTOMER SERVICE</div>
+        <div className="text-xs md:text-sm text-center lg:text-start">Friendly 24/7 customer support</div>
       </div>
 
       <div className="flex flex-col gap-2 items-center">
@@ -36,8 +36,8 @@ const KeyFeatures = () => {
             </div>
           </div>
         </div>
-        <div className="text-xl font-semibold mt-4">MONEY BACK GUARANTEE</div>
-        <div className="text-sm">We reurn money within 30 days</div>
+        <div className="text-base md:text-xl font-semibold mt-4 text-center lg:text-start">MONEY BACK GUARANTEE</div>
+        <div className="text-xs md:text-sm text-center lg:text-start">We reurn money within 30 days</div>
       </div>
     </div>
   );
