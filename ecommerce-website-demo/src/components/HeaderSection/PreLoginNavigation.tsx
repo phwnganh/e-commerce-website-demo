2f94df06057b29f1e7be550f7c80158c6ed383f5
phwnganh <phuonganh25102003@gmail.com>
2025-12-09 16:36:44 +0700

    feat: responsive prelogin header section

--- ecommerce-website-demo/src/components/HeaderSection/PreLoginNavigation.tsx
+++ ecommerce-website-demo/src/components/HeaderSection/PreLoginNavigation.tsx
@@ -3,12 +3,21 @@ import { HOMEPAGE, LOGIN, SIGNUP } from "../../constants/route.constants";
 
 const PreLoginNavigation = () => {
   return (
-    <section className="flex flex-row justify-between items-center py-[7px]">
+    <section className="flex flex-row justify-between items-center py-[7px] px-4 lg:p-0">
+      <button className="md:hidden block">
+        <svg width="24" height="24" stroke="currentColor">
+          <path d="M3 6h18M3 12h18M3 18h18" />
+        </svg>
+      </button>
       <h3 className="font-bold text-2xl">Exclusive</h3>
-      <div className="flex gap-12 items-center">
+      <div className="hidden lg:flex gap-12 items-center">
         <NavLink
           className={({ isActive }) =>
-            `${isActive ? "border-b-2 border-b-[#00000080]" : "hover:border-b-2 border-b-[#00000080]"}`
+            `${
+              isActive
+                ? "border-b-2 border-b-[#00000080]"
+                : "hover:border-b-2 border-b-[#00000080]"
+            }`
           }
           to={HOMEPAGE}
         >
@@ -19,19 +28,21 @@ const PreLoginNavigation = () => {
         <NavLink
           to={LOGIN}
           className={({ isActive }) =>
-            `${isActive ? "border-b-2 border-b-[#00000080]" : "hover:border-b-2"}`
+            `${
+              isActive ? "border-b-2 border-b-[#00000080]" : "hover:border-b-2"
+            }`
           }
         >
           Login
         </NavLink>
       </div>
 
-      <div className=" bg-[#F5F5F5] py-1.75 flex gap-8.5 pl-5 pr-3 rounded-sm">
+      <div className=" bg-[#F5F5F5] py-1.75 hidden sm:flex gap-8.5 pl-5 pr-3 rounded-sm">
         <div className="min-w-[153px]">
           <input
             type="text"
             placeholder="What are you looking for?"
-            className="text-xs w-full"
+            className="text-xs w-full outline-none"
           />
         </div>
 
@@ -42,7 +53,7 @@ const PreLoginNavigation = () => {
           fill="none"
           stroke="currentColor"
           strokeWidth="2"
-          className="shrink-0"
+          className="shrink-0 cursor-pointer"
         >
           <circle cx="11" cy="11" r="8" />
           <path d="m21 21-4.35-4.35" />
