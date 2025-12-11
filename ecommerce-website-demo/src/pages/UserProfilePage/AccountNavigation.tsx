d4381d1ec27488ddd1372acf60e5b5ca69cf03fe
phwnganh <phuonganh25102003@gmail.com>
2025-12-11 09:39:16 +0700

    feat: responsive account ui

--- ecommerce-website-demo/src/pages/UserProfilePage/AccountNavigation.tsx
+++ ecommerce-website-demo/src/pages/UserProfilePage/AccountNavigation.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AccountNavigation = () => {
   return (
-    <section className="flex flex-col gap-4">
+    <section className="hidden lg:flex lg:flex-col gap-4">
       <p className="font-medium">Manage My Account</p>
       <div className="flex flex-col indent-8.5 gap-2">
         <p className="text-[#00000080]">My Profile</p>
