b4905d01253881291e4e339d194ea341237e186c
phwnganh <phuonganh25102003@gmail.com>
2025-12-13 01:37:38 +0700

    fix: insert breadcumb in contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,9 +1,25 @@
+import { NavLink } from "react-router-dom";
 import ContactFormSection from "./ContactFormSection";
 import ContactInfoSection from "./ContactInfoSection";
+import { CONTACT, HOMEPAGE } from "../../constants/route.constants";
 
 const ContactPage = () => {
   return (
     <main className="max-w-[1170px] mx-auto">
+      <div className="mt-20 flex flex-row gap-3 items-center">
+        <NavLink to={HOMEPAGE} className="text-[#00000080] text-sm">
+          Home
+        </NavLink>
+        <div className="border border-[#00000080] rotate-[117.05deg] w-3 h-0"></div>
+        <NavLink
+          to={CONTACT}
+          className={({ isActive }) =>
+            `${isActive ? "text-sm" : "text-[#00000080] text-sm"}`
+          }
+        >
+          Contact
+        </NavLink>
+      </div>
       <div className="flex gap-[30px] mt-20 mb-35">
         <ContactInfoSection />
         <ContactFormSection />
