7718c56fab67d7385f9e3a059557d0b33e87d9bf
phwnganh <phuonganh25102003@gmail.com>
2025-12-15 23:12:00 +0700

    refactor: remove login/signup section

--- src/pages/PreLoginPage/LoginPage.tsx
+++ src/pages/PreLoginPage/LoginPage.tsx
@@ -1,9 +1,84 @@
-import LoginSection from "./LoginSection";
+import { useState, type FormEvent } from "react";
+import { useSetAtom } from "jotai";
+import { accessTookenAtom, userAtom } from "../../atom/store";
+import { useNavigate } from "react-router-dom";
+import type { LoginResponse } from "../../types/AuthType";
+import { HOMEPAGE } from "../../constants/route.constants";
+import PreLoginComponent from "../../components/ui/PreLoginComponent";
+import PrimaryCustomButton from "../../components/ui/PrimaryCustomButton";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const setUser = useSetAtom(userAtom);
+  const setToken = useSetAtom(accessTookenAtom);
+  const navigate = useNavigate();
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          expiresInMins: 30, // optional, defaults to 60
+        }),
+        // credentials: "include",
+      });
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message);
+      }
+
+      const data: LoginResponse = await res.json();
+      setUser(data);
+      setToken(data.accessToken);
+      navigate(HOMEPAGE);
+    } catch (error: any) {
+      console.log(error);
+
+      setError(error.message);
+    }
+  };
   return (
     <main>
-      <LoginSection />
+      <PreLoginComponent>
+        <h3 className="text-2xl md:text-4xl">Log in to Exclusive</h3>
+        <p className="text-sm md:text-base">Enter your details below</p>
+        <form className="mt-12 flex flex-col gap-10" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="border-b-2 opacity-50 pb-2 focus:outline-none"
+            placeholder="Username"
+          />
+          <div className="flex flex-col gap-1">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-b-2 opacity-50 pb-2 focus:outline-none"
+              placeholder="Password"
+            />
+            <p className="text-button-2">{error}</p>
+          </div>
+
+          <div className="flex flex-row gap-21.5 items-center">
+            <PrimaryCustomButton type="submit">Log In</PrimaryCustomButton>
+            <a href="#" className="text-button-2 text-sm md:text-base">
+              Forget Password?
+            </a>
+          </div>
+        </form>
+      </PreLoginComponent>
     </main>
   );
 };
--- src/pages/PreLoginPage/SignupPage.tsx
+++ src/pages/PreLoginPage/SignupPage.tsx
@@ -1,9 +1,61 @@
-import SignupSection from "./SignupSection";
+import PreLoginComponent from "../../components/ui/PreLoginComponent";
+import PrimaryCustomButton from "../../components/ui/PrimaryCustomButton";
+import googleIcon from "../../assets/Icon-Google.svg";
+import { LOGIN } from "../../constants/route.constants";
 
 const SignupPage = () => {
   return (
     <main>
-      <SignupSection />
+      <PreLoginComponent>
+        <h3 className="text-2xl md:text-4xl">Create an account</h3>
+        <p className="text-sm md:text-base">Enter your details below</p>
+
+        <form className="mt-12 flex flex-col gap-10 w-full" method="post">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="border-b-2 opacity-50 pb-2 focus:outline-none"
+            placeholder="Name"
+          />
+
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            placeholder="Email or Phone Number"
+            className="border-b-2 opacity-50 pb-2 focus:outline-none"
+          />
+
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            className="border-b-2 opacity-50 pb-2"
+          />
+          <div className="flex flex-col gap-4">
+            <PrimaryCustomButton type="submit">
+              Create Account
+            </PrimaryCustomButton>
+            <button className="flex flex-row gap-4 justify-center border border-black-opacity-40 rounded-sm py-4">
+              <img src={googleIcon} alt="google-icon" />
+              <span>Sign up with Google</span>
+            </button>
+            <div className="mt-4 flex flex-row gap-4 justify-center">
+              <p className="text-sm md:text-base text-black-opacity-70">
+                Already have account?
+              </p>
+              <a
+                className="text-sm text-black-opacity-70 font-medium md:text-base border-b border-transparent hover:border-b-black-opacity-80"
+                href={LOGIN}
+              >
+                Login
+              </a>
+            </div>
+          </div>
+        </form>
+      </PreLoginComponent>
     </main>
   );
 };
