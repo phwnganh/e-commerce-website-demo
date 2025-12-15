354d8d049eb990df2d7d27e63ad91e6e930c6bc8
phwnganh <phuonganh25102003@gmail.com>
2025-12-15 16:09:54 +0700

    fix: insert navigate to product list on wishlist section

--- src/pages/WishlistPage/RelatedProducts.tsx
+++ src/pages/WishlistPage/RelatedProducts.tsx
@@ -1,10 +1,12 @@
-import type {Products } from "../../types/ProductTypes";
+import type { Products } from "../../types/ProductTypes";
 import SecondaryCustomButton from "../../components/ui/SecondaryCustomButton";
 import React from "react";
 import WishlistRelatedProductItem from "../../components/ProductItem/WishlistRelatedProductItem";
+import { useNavigate } from "react-router-dom";
+import { PRODUCTPAGE } from "../../constants/route.constants";
 
 const RelatedProducts = ({ products }: { products: Products[] }) => {
-
+  const navigate = useNavigate();
   return (
     <section className="mt-15 mb-15 md:mb-35 px-4 lg:px-0">
       <div className="flex flex-row justify-between items-center">
@@ -12,13 +14,15 @@ const RelatedProducts = ({ products }: { products: Products[] }) => {
           <div className="bg-button-2 w-5 h-10 rounded-sm"></div>
           <p className="text-base md:text-xl font-semibold">Just For You</p>
         </div>
-        <SecondaryCustomButton>See All</SecondaryCustomButton>
+        <SecondaryCustomButton onClick={() => navigate(PRODUCTPAGE)}>
+          See All
+        </SecondaryCustomButton>
       </div>
 
       <div className="mt-15 grid grid-cols-2 md:grid-cols-4 gap-7">
         {products.slice(0, 4).map((product) => (
           <React.Fragment key={product.id}>
-            <WishlistRelatedProductItem product={product}/>
+            <WishlistRelatedProductItem product={product} />
           </React.Fragment>
         ))}
       </div>
