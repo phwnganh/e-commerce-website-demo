ff115e4daa5276ea79704239ad81be4264671e56
phwnganh <phuonganh25102003@gmail.com>
2025-12-21 17:11:53 +0700

    fix: display all name and role of our team

--- src/components/AboutUsComponent/OurTeamSection.tsx
+++ src/components/AboutUsComponent/OurTeamSection.tsx
@@ -23,7 +23,7 @@ const OurTeamSection = () => {
     }
   ]
   return (
-    <section className="mt-15 md:mt-35 grid grid-cols-3 gap-7.5">
+    <section className="mt-15 md:mt-35 grid grid-cols-3 gap-4 md:gap-7.5">
       {TEAM_MEMBERS.map((member, index) => (
       <div className="flex flex-col gap-2 md:gap-8" key={index}>
         <div className="rounded-sm bg-secondary-2 aspect-3/4 overflow-hidden pt-2 md:pt-9.5">
@@ -34,10 +34,10 @@ const OurTeamSection = () => {
           />
         </div>
         <div className="flex flex-col gap-1 md:gap-2">
-          <h4 className="font-medium text-base md:text-3xl line-clamp-1">
+          <h4 className="font-medium text-base md:text-3xl">
             {member.name}
           </h4>
-          <p className="text-xs md:text-base line-clamp-1">{member.role}</p>
+          <p className="text-xs md:text-base">{member.role}</p>
           <div className="mt-1 md:mt-4 flex gap-2 md:gap-4">
             <img src={TwitterIcon} alt="twitter-icon" className="w-4 h-4 md:w-6 md:h-6"/>
             <img src={InstagramIcon} alt="instagram-icon" className="w-4 h-4 md:w-6 md:h-6"/>
