5a06ed8fa560e2f3b5c4de0d868b0004de5b790c
phwnganh <phuonganh25102003@gmail.com>
2025-12-24 18:17:57 +0700

    feat: add animation on each hero banner

--- src/components/HomeComponent/Banner.tsx
+++ src/components/HomeComponent/Banner.tsx
@@ -1,14 +1,56 @@
 import CategoriesNavigation from "./CategoriesNavigation";
-import HeroBanner from "./HeroBanner";
 import { useAtomValue } from "jotai";
 import { categoriesNavigationAtom } from "../../atom/store";
-
+import MainHeroBanner from "../../assets/hero-banner.png";
+import Iphone16 from "../../assets/iphone-16-advertisment.jpg";
+import Iphone13 from "../../assets/iphone-13-advertisement.png";
 const Banner = () => {
   const categoriesNavigation = useAtomValue(categoriesNavigationAtom);
   return (
     <section className="flex lg:flex-row md:flex-col flex-col gap-11 px-4 lg:px-0">
       <CategoriesNavigation categories={categoriesNavigation} />
-        <HeroBanner />
+      {/* carousel */}
+      <div className="w-full overflow-hidden">
+        {/* container */}
+        <div className="flex animate-marquee">
+          <div className="flex gap-2 mt-10">
+            <img
+              src={MainHeroBanner}
+              alt="main-hero-banner"
+              className="max-w-none w-auto h-64 object-cover"
+            />
+            {/* <HeroBanner/> */}
+            <img
+              src={Iphone16}
+              alt="iphone-16-advertisement"
+              className="max-w-none w-auto h-64 object-cover"
+            />
+            <img
+              src={Iphone13}
+              alt="iphone-13-advertisement"
+              className="max-w-none w-auto h-64 object-cover"
+            />
+          </div>
+
+          {/* <div className="flex gap-2">
+            <img
+              src={MainHeroBanner}
+              alt="main-hero-banner"
+              className="max-w-none w-auto h-64 object-cover"
+            />
+            <img
+              src={Iphone16}
+              alt="iphone-16-advertisement"
+              className="max-w-none w-auto h-64 object-cover"
+            />
+            <img
+              src={Iphone13}
+              alt="iphone-13-advertisement"
+              className="max-w-none w-auto h-64 object-cover"
+            />
+          </div> */}
+        </div>
+      </div>
     </section>
   );
 };
